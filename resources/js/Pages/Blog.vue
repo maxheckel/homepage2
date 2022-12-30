5349e9f23773d5051597cdf2730f16e8e13db2ca
Max Heckel <maxheckel@maxs-mbp.lan>
2022-12-30 12:16:45 -0500

    BAsically everything is done

--- resources/js/Pages/Blog.vue
+++ resources/js/Pages/Blog.vue
@@ -1,7 +1,11 @@
 <script setup>
 import {Head} from '@inertiajs/inertia-vue3';
 import UnauthedLayout from "../Layouts/UnauthedLayout.vue";
+import SecondaryButton from "../Components/SecondaryButton.vue";
 
+const props = defineProps({
+    posts: Object
+})
 
 </script>
 
@@ -11,7 +15,18 @@ import UnauthedLayout from "../Layouts/UnauthedLayout.vue";
     <UnauthedLayout>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                awegwe
+                <div v-for="post in posts.data" class="w-full flex my-8 items-center bg-white p-4 rounded-lg shadow-sm">
+                    <div>
+                        <h2 class="text-xl"> {{post.title}}</h2>
+                        <sub class="block mt-1 italic text-gray-400 mb-4">Published: {{post.published_at}}</sub>
+                        {{post.short_content}}
+                    </div>
+
+
+                    <div class="ml-auto">
+                        <a :href="route('blog.show', post.id+'-'+post.slug)"> <SecondaryButton>View</SecondaryButton></a>
+                    </div>
+                </div>
             </div>
         </div>
 
---
+++ resources/js/Pages/Post.vue
@@ -0,0 +1,53 @@
+<script setup>
+import {Head} from '@inertiajs/inertia-vue3';
+import UnauthedLayout from "@/Layouts/UnauthedLayout.vue";
+import ProjectCard from "@/Components/ProjectCard.vue";
+import PrimaryButton from "../Components/PrimaryButton.vue";
+import SecondaryButton from "../Components/SecondaryButton.vue";
+
+const props = defineProps({
+    post: Object
+})
+</script>
+
+<template>
+    <Head :title="post.title"/>
+
+    <UnauthedLayout>
+
+        <div class="max-w-4xl mx-auto p-4 sm:px-6 lg:px-8">
+
+            <div class="mt-8 flex">
+                <div>
+                    <h1 class="text-4xl block">{{ post.title }}</h1>
+                    <sub class="italic">Published: {{post.published_at}} Updated: {{post.updated_at}}</sub>
+                </div>
+
+            </div>
+
+            <div class="content my-8" v-html="post.content"></div>
+            <!-- Go to www.addthis.com/dashboard to customize your tools --> <div class="addthis_inline_share_toolbox"></div>
+        </div>
+
+
+    </UnauthedLayout>
+</template>
+
+<style lang="scss">
+
+.content{
+    @apply text-lg leading-loose;
+    h1{
+        @apply text-3xl;
+    }
+    h2{
+        @apply text-2xl;
+    }
+    h3{
+        @apply text-xl;
+    }
+    b{
+        @apply font-bold;
+    }
+}
+</style>
