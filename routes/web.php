d441aaff70e53eaa963d3447fc90605ced0b9a80
Max Heckel <maxheckel@maxs-mbp.lan>
2023-06-23 10:04:39 -0400

    App

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,24 @@ Route::get('/projects/{slug}', [ProjectController::class, 'show'])->name('projec
 Route::get('/blog/{slug}', [BlogController::class, 'show'])->name('blog.show');
 
 
+Route::any('/halo/collectors/{id}/poll', function(){
+    return `<ReceiveMessageResponse xmlns="http://queue.amazonaws.com/doc/2012-11-05/">
+    <ReceiveMessageResult>
+        <Message>
+            <MessageId>e15b23be-b568-414a-8601-658faa4c9f08</MessageId>
+            <ReceiptHandle>32aed65d-aa5d-471c-bfb1-8107e527b253</ReceiptHandle>
+            <MD5OfBody>794c0eb14afd5868408e25e1ed5173ef</MD5OfBody>
+            <Body>
+                {"iv":"9acb1e2076e423d3148951c2ecb39fbb","authTag":"84e2c7d254480281caa92bead3dfa533","key":"38fd38b12417544f5abfceb9b98be3b6830af2a96770928daae778080d7b61a0ea46d14c153cfe567f7e5cf2303d72c8f1bc3c33ce4e256176fdbdbf33e8208b6a5da603dc0e9e0a7c80ec85b986e0af3cd7dc2c9172e36d08df6dc6e8071fb4343a0a23ee4f55d40fc72f5f9bc772b965a4b7846ae95fc1d8d843654ec4e767a587576239f5e66c5ec43d5d610703bbeb398e424531748c68433411834815e337d432bf083c780ee87cb6776eb94d8da568810a01fdab540fbc2acfdb66af04a2953ce66d0b7050852a731fc5328004faf1f4a6fe3f6daaf568205c618a4608af103f961504f45fec2f2d79c5f172142e20c2d5bc0f0e796cc57d0a1591ee6f75598d951366e210a9f91b307dba6811e26fb6fb2a9b560c0c61522745c4506f6975628571f3b13a92b7e6f0b01b6c3f54bf4e7b5d458f4ce10641e95067cf10c51d1011c1a1f1b21af7d9e6833ccafca7960eda2f5cd19c205a7c84f565994c5850eaf3e4d560ac16cba93aff67b584c9a6090042152b458ab20288c835fca7bd21a180b56f0184b862be9ecabf0ca88f249dc7aecfbf8e6970b0b7be87a7568fdfc6c8052ef8e1e0ef59133e0cf9ea66c6d43bb99ed331b56dfdf9669a44bd60f6f4f348cd44a623417dede416a9363e39ab8737d496ee637e1bf2dc5c0cf967d871162e3cfd7e5927d24c4c89daa0e6e4d261cf6d9c8fd3b618da4cb02916","message":"f244df9c390fb506ac25"}
+            </Body>
+        </Message>
+    </ReceiveMessageResult>
+    <ResponseMetadata>
+        <RequestId>dfa2c546-b02b-49c8-a3d4-8d9f5d0f4085</RequestId>
+    </ResponseMetadata>
+</ReceiveMessageResponse>`;
+});
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
